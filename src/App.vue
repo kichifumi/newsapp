2feabd30924320ded4ac5551b65f339afdfe0491
kichifumi <kichifumi@gmail.com>
2021-02-24 15:13:56 +0900

    不要コード削除

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <div id="nav">
       <div class="tabs">
-      <!-- <TabItem v-for="item in list" v-bind="item" :key="item.id" v-model="currentId"/> -->
       </div>
       <Headers />
       <router-link to="/">Headline</router-link> |
@@ -20,29 +19,12 @@
 
 <script>
 import Headers from "@/components/Headers.vue"; // @ is an alias to /src
-// import TabItem from '@/components/TabItem.vue'
 
 export default {
   name: "App",
   components: {
     Headers,
-    // TabItem
   },
-  // computed: {
-  //   current() {
-  //     return this.list.find(el => el.id === this.currentId) || {}
-  //   }
-  // },
-  // data() {
-  //   return {
-  //     currentId: 1,
-  //     list: [
-  //       { id: 1, label: 'Headline', content: '' },
-  //       { id: 2, label: 'Business', content: 'business' },
-  //       { id: 3, label: 'Entertainment', content: 'コンテンツ3' },
-  //     ]
-  //   }
-  // },
 };
 </script>
 
